d8d5420a114c47a687e77894897522390ca19ede
nMijatovic <nMijatovic@users.noreply.github.com>
2015-06-02 02:37:41 +0200

    Create time.c

---
+++ time.c
@@ -0,0 +1,32 @@
+#include "tank.h"
+
+int delay_s(int timeToDelay, unsigned short* mm, unsigned short* pp, int *check){
+	if (*check == 1){
+		return 1;
+	}
+	if (*mm - *pp > 0){
+		if (*mm - *pp >= timeToDelay)	{
+			return 1;
+		}
+	}
+	else if (*mm - *pp < 0){
+		if (1000 - *pp + *mm >= timeToDelay){
+			return 1;
+		}
+	}
+	return 0;
+}
+
+void time_now(){
+	time_t pIntTime;
+	struct tm* currentLocalTime;
+	char* dateTimeString = (char*)calloc(100 + 1, sizeof(char));
+	pIntTime = time(NULL);
+	currentLocalTime = localtime(&pIntTime);
+	attron(COLOR_PAIR(1));
+	if (currentLocalTime && dateTimeString)
+		strftime(dateTimeString, 100, "%H:%M:%S", currentLocalTime);
+	mvaddstr(1, 42, dateTimeString);
+	refresh();
+	free(dateTimeString);
+}
