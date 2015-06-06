b13f08b6216dc1fcc3b63f5af95fb6e9baa1ec36
vasa137 <vasa.nikola@gmail.com>
2015-06-06 15:53:43 +0200

    Create movement.c

---
+++ movement.c
@@ -0,0 +1,139 @@
+#include "tank.h"
+
+int lvl_can_move(int yu, int xu, int yd, int xd, int num){
+	switch (num){
+	case 1: if ((yu - 1) == 2) return 0; else return 1; break;
+	case 2: if ((yd + 1) == (dimx + 2)) return 0; else return 1; break;
+	case 3: if ((xd - 1) == 2) return 0; else return 1; break;
+	case 4: if ((xu + 1) == (dimy + 2)) return 0; else return 1; break;
+	}
+}  // ispituje mogucnost kretanje u zadatom smeru(zidovi mape ogranicavaju kretanje)
+
+void move_up2x2(int yu, int xu, int yd, char xd){
+	int i, j;
+	for (j = yd, i = xd; i <= xu; i++)
+		what_to_print(j, i, j, i, lvl_matrix[j - 3][i - 3]);
+	for (j = yu - 1, i = xd; i <= xu; i++)
+		what_to_print(j, i, j, i, element);
+	refresh();
+
+}
+
+void move_right2x2(int yu, int xu, int yd, int xd){
+	int i, j;
+
+	for (j = yu, i = xd; j <= yd; j++){
+		what_to_print(j, i, j, i, lvl_matrix[j - 3][i - 3]);
+	}
+	for (j = yu, i = xu + 1; j <= yd; j++)
+		what_to_print(j, i, j, i, element);
+	refresh();
+}
+
+void move_down2x2(int yu, int xu, int yd, int xd){
+	int i, j;
+	for (j = yu, i = xd; i <= xu; i++){
+		what_to_print(j, i, j, i, lvl_matrix[j - 3][i - 3]);
+	}
+	for (j = yd + 1, i = xd; i <= xu; i++){
+		what_to_print(j, i, j, i, element);
+	}
+	refresh();
+}
+
+void move_left2x2(int yu, int xu, int yd, int xd){
+	int i, j;
+	for (i = xu, j = yu; j <= yd; j++)
+		what_to_print(j, i, j, i, lvl_matrix[j - 3][i - 3]);
+	for (i = xd - 1, j = yu; j <= yd; j++)
+		what_to_print(j, i, j, i, element);
+	refresh();
+}
+
+void expand_y(int *yu, int xu, int *yd, int xd){
+	int i, j;
+	if ((*yd - *yu) == 16) return;
+	--(*yu); ++(*yd);
+	for (j = *yu, i = xd; i <= xu; i++){
+		what_to_print(j, i, j, i, element);
+		what_to_print(*yd, i, *yd, i, element);
+	}
+	refresh();
+} // uvecaj po visni
+
+void reduce_y(int *yu, int xu, int *yd, int xd){
+	int i, j;
+	if (*yu == *yd) return;
+	attron(COLOR_BLACK);
+	for (i = *yu, j = xd; j <= xu; j++){
+		what_to_print(*yu, j, *yu, j, lvl_matrix[i - 2][j - 2]);
+		what_to_print(*yd, j, *yd, j, lvl_matrix[*yd - 2][j - 2]);
+	}
+	(*yu)++;
+	(*yd)--;
+	attroff(COLOR_BLACK);
+	refresh();
+}  // smanji po visini
+
+void reduce_x(int yu, int *xu, int yd, int *xd){
+	int i;
+	if (*xd == *xu) return;
+	attron(COLOR_BLACK);
+	for (i = yu; i <= yd; i++){
+		what_to_print(i, *xd, i, *xd, lvl_matrix[i - 2][*xd - 2]);
+		what_to_print(i, *xu, i, *xu, lvl_matrix[i - 2][*xu - 2]);
+	}
+	++(*xd);
+	--(*xu);
+	attroff(COLOR_BLACK);
+	refresh();
+}  // smanji po duzini
+
+void expand_x(int yu, int *xu, int yd, int *xd){
+	int i;
+	if ((*xu - *xd) == 16) return;
+	++(*xu); --(*xd);
+	for (i = yu; i <= yd; i++){
+		what_to_print(i, *xu, i, *xu, element);
+		what_to_print(i, *xd, i, *xd, element);
+	}
+	refresh();
+} // uvecaj po duzini
+
+void menu_up(int *mv, int limit, int *ind, int from, int mainm){
+	int i;
+	if (*mv == limit) return;
+	*mv = *mv - 2;
+	attron(COLOR_PAIR(8));
+	if(mainm) mvprintw(*mv, from, meni[--(*ind)]); //koji meni printa
+	else mvprintw(*mv, from, lvl_meni[--(*ind)]);
+	attroff(COLOR_PAIR(8));
+	if(mainm) mvprintw(*mv + 2, from, meni[*ind + 1]);
+	else mvprintw(*mv + 2, from, lvl_meni[*ind + 1]);
+	refresh();
+} // kretanje po meniju
+
+void menu_down(int *mv, int limit, int *ind, int from, int mainm){
+	int i;
+	if (*mv == limit) return;
+	*mv = *mv + 2;
+	attron(COLOR_PAIR(8));
+	if(mainm) mvprintw(*mv, from, meni[++(*ind)]);
+	else mvprintw(*mv, from, lvl_meni[++(*ind)]);
+	attroff(COLOR_PAIR(8));
+	if(mainm) mvprintw(*mv - 2, from, meni[*ind - 1]);
+	else mvprintw(*mv - 2, from, lvl_meni[*ind - 1]);
+	refresh();
+} // kretanje po meniju
+
+void size_resize(int *yu, int *xu, int *yd, int *xd){
+	while (1){
+		switch (getch()){
+		case KEY_UP: if (lvl_can_move(*yu, *xu, *yd, *xd, 1) && lvl_can_move(*yu, *xu, *yd, *xd, 2)) expand_y(yu, *xu, yd, *xd);   break;
+		case KEY_DOWN: reduce_y(yu, *xu, yd, *xd); break;
+		case KEY_LEFT: reduce_x(*yu, xu, *yd, xd); break;
+		case KEY_RIGHT: if (lvl_can_move(*yu, *xu, *yd, *xd, 3) && lvl_can_move(*yu, *xu, *yd, *xd, 4)) expand_x(*yu, xu, *yd, xd); break;
+		case 'r': case 'R': return; break;
+		}
+	}
+}
