b404173bb8ec10c66b0fda327b152a4797a444a9
nMijatovic <nMijatovic@users.noreply.github.com>
2015-06-20 22:53:54 +0200

    Update main.c

--- main.c
+++ main.c
@@ -8,18 +8,20 @@ chtype matrix[dimx + 1][dimy + 1];                         // velicina mape
 
 chtype element = 'b';
 
-int top[] = { 0, 1, 2, 3, 4, 5, 6, 7, 8 };
-int left[] = { 2, 5, 8, 1, 4, 7, 0, 3, 6 };
-int right[] = { 6, 3, 0, 7, 4, 1, 8, 5, 2 };
+Lst *lst;
+
+int top[]    = { 0, 1, 2, 3, 4, 5, 6, 7, 8 };
+int left[]   = { 2, 5, 8, 1, 4, 7, 0, 3, 6 };
+int right[]  = { 6, 3, 0, 7, 4, 1, 8, 5, 2 };
 int bottom[] = { 8, 7, 6, 5, 4, 3, 2, 1, 0 };
 
 int visit_grass[3][3]={0,0,0,0,0,0,0,0,0};
 
-tank special_tank_v[] = { { ACS_VLINE, 1 }, { ACS_VLINE, 1 }, { ACS_VLINE, 1 }, { ACS_VLINE, 1 }, { ACS_BULLET, 5 }, { ACS_VLINE, 1 }, { ACS_DIAMOND, 4 }, { ACS_HLINE, 4 }, { ACS_DIAMOND, 4 } };
-tank special_tank_h[] = { { ACS_HLINE, 1 }, { ACS_HLINE, 1 }, { ACS_HLINE, 1 }, { ACS_HLINE, 1 }, { ACS_BULLET, 5 }, { ACS_HLINE, 1 }, { ACS_DIAMOND, 4 }, { ACS_VLINE, 4 }, { ACS_DIAMOND, 4 } };
+TankDesign special_tank_v[] = { { ACS_VLINE, 1 }, { ACS_VLINE, 1 }, { ACS_VLINE, 1 }, { ACS_VLINE, 1 }, { ACS_BULLET, 5 }, { ACS_VLINE, 1 }, { ACS_DIAMOND, 4 }, { ACS_HLINE, 4 }, { ACS_DIAMOND, 4 } };
+TankDesign special_tank_h[] = { { ACS_HLINE, 1 }, { ACS_HLINE, 1 }, { ACS_HLINE, 1 }, { ACS_HLINE, 1 }, { ACS_BULLET, 5 }, { ACS_HLINE, 1 }, { ACS_DIAMOND, 4 }, { ACS_VLINE, 4 }, { ACS_DIAMOND, 4 } };
 
-tank normal_tank_v[] = { { 126, 6 }, { ACS_VLINE, 1 }, { 126, 6 }, { ACS_DIAMOND, 2 }, { 79, 7 }, { ACS_DIAMOND, 2 }, { ACS_DIAMOND, 2 }, { ACS_CKBOARD, 8 }, { ACS_DIAMOND, 2 } };
-tank normal_tank_h[] = { { 126, 6 }, { ACS_HLINE, 1 }, { 126, 6 }, { ACS_DIAMOND, 2 }, { 79, 7 }, { ACS_DIAMOND, 2 }, { ACS_DIAMOND, 2 }, { ACS_CKBOARD, 8 }, { ACS_DIAMOND, 2 } };
+TankDesign normal_tank_v[] = { { 126, 6 }, { ACS_VLINE, 1 }, { 126, 6 }, { ACS_DIAMOND, 2 }, { 79, 7 }, { ACS_DIAMOND, 2 }, { ACS_DIAMOND, 2 }, { ACS_CKBOARD, 8 }, { ACS_DIAMOND, 2 } };
+TankDesign normal_tank_h[] = { { 126, 6 }, { ACS_HLINE, 1 }, { 126, 6 }, { ACS_DIAMOND, 2 }, { 79, 7 }, { ACS_DIAMOND, 2 }, { ACS_DIAMOND, 2 }, { ACS_CKBOARD, 8 }, { ACS_DIAMOND, 2 } };
 
 void main(){
 	unsigned short mm, pp;
@@ -32,9 +34,12 @@ void main(){
     clock_t start1 = clock();
     clock_t end= clock();
     clock_t end1 = clock();
+	lst->first = NULL;
+	lst->last = NULL;
 	init_curses();
 	create_map(map_name);
 	main_menu(1);
+	alloc_tank();
 	create_tank(y, x, 1, special_tank_v);
 	last_move = 1; //zato sto tenk stvaramo uspravno
 	while (1){
