346f07121643624e1fc4cf14938296641df967c5
nMijatovic <nMijatovic@users.noreply.github.com>
2015-06-20 22:54:40 +0200

    Update tank_logic.c

--- tank_logic.c
+++ tank_logic.c
@@ -1,6 +1,44 @@
 #include "tank.h"
 
-void move_tank(int y, int x, int mov, tank* ver, tank* hor){
+
+void alloc_tank(){
+	List *novi;
+	novi = (List*)malloc(sizeof(List));
+	novi->next = NULL;
+	if (lst->first == NULL){
+		lst->first = novi;
+	}
+	else lst->last->next = novi;
+	lst->last = novi;
+}
+
+void action(int *y, int *x, TankDesign* ver, TankDesign* hor, int keyPressed, int *check, int *last_move, int *projectil_dir){
+	switch (keyPressed)
+	{
+	case KEY_UP:    *last_move = 1; if (can_move(*y - 2, *x, 1)) move_tank(--*y, *x, KEY_UP, ver, hor);    else { delete_tank(*y, *x);  create_tank(*y, *x, 1, ver); } refresh(); break;
+	case KEY_LEFT:  *last_move = 2; if (can_move(*y, *x - 2, 2)) move_tank(*y, --*x, KEY_LEFT, ver, hor);  else { delete_tank(*y, *x);  create_tank(*y, *x, 2, hor); } refresh(); break;
+	case KEY_RIGHT: *last_move = 3; if (can_move(*y, *x + 2, 3)) move_tank(*y, ++*x, KEY_RIGHT, ver, hor); else { delete_tank(*y, *x);  create_tank(*y, *x, 3, hor); } refresh(); break;
+	case KEY_DOWN:  *last_move = 4; if (can_move(*y + 2, *x, 4)) move_tank(++*y, *x, KEY_DOWN, ver, hor);  else { delete_tank(*y, *x);  create_tank(*y, *x, 4, ver); } refresh(); break;
+	case ' ':       if (!(*check)) { *projectil_dir = *last_move;  *check = 1; } break;
+	}
+}
+
+int can_move(int y, int x, int barrel) {
+	switch (barrel)
+	{
+	case 1: case 4:
+		if (((matrix[y - y1b][x - x1b - 1] == ' ') || (matrix[y - y1b][x - x1b - 1] == 'g')) &&
+			((matrix[y - y1b][x - x1b] == ' ') || (matrix[y - y1b][x - x1b] == 'g')) &&
+			((matrix[y - y1b][x - x1b + 1] == ' ') || (matrix[y - y1b][x - x1b + 1] == 'g'))) return 1; else return 0;
+
+	case 2: case 3:
+		if (((matrix[y - y1b - 1][x - x1b] == ' ') || (matrix[y - y1b - 1][x - x1b] == 'g')) &&
+			((matrix[y - y1b][x - x1b] == ' ') || (matrix[y - y1b][x - x1b] == 'g')) &&
+			((matrix[y - y1b + 1][x - x1b] == ' ') || (matrix[y - y1b + 1][x - x1b] == 'g'))) return 1; else return 0;
+	}
+}
+
+void move_tank(int y, int x, int mov, TankDesign* ver, TankDesign* hor){
 	switch (mov){
 	case KEY_UP:    delete_tank(y + 1, x);  create_tank(y, x, 1, ver);  break;
 	case KEY_LEFT:  delete_tank(y, x + 1);  create_tank(y, x, 2, hor);  break;
@@ -9,28 +47,6 @@ void move_tank(int y, int x, int mov, tank* ver, tank* hor){
 	}
 }
 
-int can_move(int y, int x, int barrel) {
-	switch (barrel)
-	{
-	case 1: case 4: 
-		if (((matrix[y - y1b][x - x1b - 1] == ' ') || (matrix[y - y1b][x - x1b - 1] == 'g')) && 
-		((matrix[y - y1b][x - x1b] == ' ') || (matrix[y - y1b][x - x1b] == 'g')) && 
-		((matrix[y - y1b][x - x1b + 1] == ' ') || (matrix[y - y1b][x - x1b + 1] == 'g'))) return 1; else return 0;
 
-	case 2: case 3: 
-		if (((matrix[y - y1b - 1][x - x1b] == ' ') || (matrix[y - y1b - 1][x - x1b] == 'g')) && 
-		((matrix[y - y1b][x - x1b] == ' ') || (matrix[y - y1b][x - x1b] == 'g')) && 
-		((matrix[y - y1b + 1][x - x1b] == ' ') || (matrix[y - y1b + 1][x - x1b] == 'g'))) return 1; else return 0;
-	}
-}
 
-void action(int *y, int *x, tank* ver, tank* hor, int keyPressed, int *check, int *last_move, int *projectil_dir){
-	switch (keyPressed)
-	{
-	case KEY_UP:    *last_move = 1; if (can_move(*y - 2, *x, 1)) move_tank(--*y, *x, KEY_UP, ver, hor);    else { delete_tank(*y, *x);  create_tank(*y, *x, 1, ver); } refresh(); break;
-	case KEY_LEFT:  *last_move = 2; if (can_move(*y, *x - 2, 2)) move_tank(*y, --*x, KEY_LEFT, ver, hor);  else { delete_tank(*y, *x); create_tank(*y, *x, 2, hor);  } refresh(); break;
-	case KEY_RIGHT: *last_move = 3; if (can_move(*y, *x + 2, 3)) move_tank(*y, ++*x, KEY_RIGHT, ver, hor); else { delete_tank(*y, *x);  create_tank(*y, *x, 3, hor); } refresh(); break;
-	case KEY_DOWN:  *last_move = 4; if (can_move(*y + 2, *x, 4)) move_tank(++*y, *x, KEY_DOWN, ver, hor);  else { delete_tank(*y, *x);  create_tank(*y, *x, 4, ver); } refresh(); break;
-	case ' ':       if (!(*check)) { *projectil_dir = *last_move;  *check = 1; } break;
-	}
-}
+
