9ab77ab4df2c69ab2a1234bcd0ddf43114fbd592
nMijatovic <nMijatovic@users.noreply.github.com>
2015-06-21 16:35:22 +0200

    Update main.c

--- main.c
+++ main.c
@@ -2,37 +2,35 @@
 
 Lst *lst;
 
-char meni[][dimx] = { " New Game ", " Load Game ", " Settings ", " Map Editor ", " Play Custom map" , " High Scores ", " Exit " };
+char meni[][dimx] = { " New Game ", " Load Game ", " Settings ", " Map Editor ", " Play Custom map", " High Scores ", " Exit " };
 
-char map_name[50] = "dobraje.txt"; 
+char map_name[50] = "dobraje.txt";
 
 chtype matrix[dimx + 1][dimy + 1];                         // velicina mape
 
 chtype element = 'b';
 
-int top[] = { 0, 1, 2, 3, 4, 5, 6, 7, 8 };
-int left[] = { 2, 5, 8, 1, 4, 7, 0, 3, 6 };
-int right[] = { 6, 3, 0, 7, 4, 1, 8, 5, 2 };
+int top[]    = { 0, 1, 2, 3, 4, 5, 6, 7, 8 };
+int left[]   = { 2, 5, 8, 1, 4, 7, 0, 3, 6 };
+int right[]  = { 6, 3, 0, 7, 4, 1, 8, 5, 2 };
 int bottom[] = { 8, 7, 6, 5, 4, 3, 2, 1, 0 };
 
 TankDesign special_tank_v[] = { { ACS_VLINE, 1 }, { ACS_VLINE, 1 }, { ACS_VLINE, 1 }, { ACS_VLINE, 1 }, { ACS_BULLET, 5 }, { ACS_VLINE, 1 }, { ACS_DIAMOND, 4 }, { ACS_HLINE, 4 }, { ACS_DIAMOND, 4 } };
 TankDesign special_tank_h[] = { { ACS_HLINE, 1 }, { ACS_HLINE, 1 }, { ACS_HLINE, 1 }, { ACS_HLINE, 1 }, { ACS_BULLET, 5 }, { ACS_HLINE, 1 }, { ACS_DIAMOND, 4 }, { ACS_VLINE, 4 }, { ACS_DIAMOND, 4 } };
-TankDesign normal_tank_v[] = { { 126, 6 }, { ACS_VLINE, 1 }, { 126, 6 }, { ACS_DIAMOND, 2 }, { 79, 7 }, { ACS_DIAMOND, 2 }, { ACS_DIAMOND, 2 }, { ACS_CKBOARD, 8 }, { ACS_DIAMOND, 2 } };
-TankDesign normal_tank_h[] = { { 126, 6 }, { ACS_HLINE, 1 }, { 126, 6 }, { ACS_DIAMOND, 2 }, { 79, 7 }, { ACS_DIAMOND, 2 }, { ACS_DIAMOND, 2 }, { ACS_CKBOARD, 8 }, { ACS_DIAMOND, 2 } };
+TankDesign normal_tank_v[]  = { { 126, 6 }, { ACS_VLINE, 1 }, { 126, 6 }, { ACS_DIAMOND, 2 }, { 79, 7 }, { ACS_DIAMOND, 2 }, { ACS_DIAMOND, 2 }, { ACS_CKBOARD, 8 }, { ACS_DIAMOND, 2 } };
+TankDesign normal_tank_h[]  = { { 126, 6 }, { ACS_HLINE, 1 }, { 126, 6 }, { ACS_DIAMOND, 2 }, { 79, 7 }, { ACS_DIAMOND, 2 }, { ACS_DIAMOND, 2 }, { ACS_CKBOARD, 8 }, { ACS_DIAMOND, 2 } };
 
-void main(){
+void main(){ // Da sredimo main kasnije.
 	struct timeb vreme;
 	int keyPressed, i;
-	clock_t start = clock();
-    clock_t end= clock();
 	lst = (Lst*)malloc(sizeof(Lst));
-	lst->first=NULL;
-	lst->curr=NULL;
-	lst->last=NULL;
-	lst->n=0;
+	lst->first = NULL;
+	lst->curr = NULL;
+	lst->last = NULL;
+	lst->n = 0;
 	init_curses();
 	alloc_tank();
-	lst->curr=lst->first;
+	lst->curr = lst->first;
 	create_map();
 	main_menu(1);
 	free_tank(lst->curr);
@@ -43,12 +41,12 @@ void main(){
 		if (_kbhit())
 		{
 			keyPressed = getch();
-			lst->curr=lst->first;
-			action(keyPressed);
+			lst->curr = lst->first;
+			action(keyPressed, &lst->first->tankAll);
 		}
 		for (i = 0, lst->curr = lst->first; i < lst->n; i++, lst->curr = lst->curr->next){
-			if (i!=0) easy_bot(&start, &lst->curr->tankAll.tank.position.barrel, &lst->curr->tankAll.tank.position.y, &lst->curr->tankAll.tank.position.x);
-			if (lst->curr->tankAll.projectile.projectPhase) projectile(lst->curr->tankAll.tank.position.y,
+			if (i != 0) easy_bot();
+			if (lst->curr->tankAll.projectile.phase) projectile(lst->curr->tankAll.tank.position.y,
 				lst->curr->tankAll.tank.position.x, lst->curr->tankAll.projectile.position.projectil_dir);
 		}
 	}
