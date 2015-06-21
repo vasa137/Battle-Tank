9bfa0013f006337313f15694a83e579fe611b488
nMijatovic <nMijatovic@users.noreply.github.com>
2015-06-21 16:38:52 +0200

    Update bot.c

--- bot.c
+++ bot.c
@@ -1,65 +1,19 @@
 #include "tank.h"
 
-void move_bot(int *z, int *t, int *bot_barrel)
-{
-        int array_bot_barrel[] = { 1, 2, 3, 4 };
-        int a = *z, b = *t;
-        int bot_barrel1 = *bot_barrel;
-        switch (*bot_barrel)
-        {
-        case 1:
-                if ((matrix[a - 4][b - 1] == ' ') && (matrix[a - 4][b - 2] == ' ') && (matrix[a - 4][b - 3] == ' '))
-                {
-                        delete_tank(a, b);
-						(*z)--;
-                        create_tank(bot_barrel1, lst->curr->tankAll);
-                        refresh();
-                }
-                else
-                        *bot_barrel = array_bot_barrel[rand() % 4];
-                break;
-        case 2:
-                if ((matrix[a - 1][b - 4] == ' ') && (matrix[a - 2][b - 4] == ' ') && (matrix[a - 3][b - 4] == ' '))
-                {
-                        delete_tank(a, b);
-						(*t)--;
-                        create_tank(bot_barrel1, lst->curr->tankAll);
-                        refresh();
-                }
-                else
-                        *bot_barrel = array_bot_barrel[rand() % 4];
-                break;
-        case 3:
-                if ((matrix[a - 1][b] == ' ') && (matrix[a - 2][b] == ' ') && (matrix[a - 3][b] == ' '))
-                {
-                        delete_tank(a, b);
-						(*t)++;
-                        create_tank(bot_barrel1, lst->curr->tankAll);
-                        refresh();
-                }
-                else
-                        *bot_barrel = array_bot_barrel[rand() % 4];
-                break;
-        case 4:
-                if ((matrix[a][b - 1] == ' ') && (matrix[a][b - 2] == ' ') && (matrix[a][b - 3] == ' '))
-                {
-                        delete_tank(a, b);
-						(*z)++;
-                        create_tank(bot_barrel1, lst->curr->tankAll);
-                        refresh();
-                }
-                else
-                        *bot_barrel = array_bot_barrel[rand() % 4];
-        }
-}
- 
-void easy_bot(clock_t *start, int *bot_barrel, int *z, int *t)
-{
-        clock_t end;
-        end = clock();
-        if ((end - (*start)) > 80)
-		{
-				move_bot(z, t, bot_barrel);
-                *start = clock();
-        }
+void easy_bot(){ // Mislim da bi bilo dobro da se fja zove bot, a da se tezina razlikuje u generatorima koje ces implementirati dole.
+	// koristi matricu da bi odredio puteve, nemoj ubacivati kodove u vec postojece fje da bi odredio put.
+	int array_bot_barrel[] = { 1, 2, 3, 4 };
+	struct timeb vreme;
+	ftime(&vreme);
+	if (lst->curr->tankAll.tank.phase == 2)	{
+		lst->curr->tankAll.tank.pp = lst->curr->tankAll.tank.mm = vreme.millitm;
+		lst->curr->tankAll.tank.phase = 0;
+	}
+	lst->curr->tankAll.tank.mm = vreme.millitm;
+	if (delay_s(120, lst->curr->tankAll.tank.mm, lst->curr->tankAll.tank.pp, lst->curr->tankAll.tank.phase)){
+		action(lst->curr->tankAll.tank.position.barrel, &lst->curr->tankAll);
+		lst->curr->tankAll.tank.position.barrel = array_bot_barrel[rand() % 4]; // Ovde neka budu dodele vrednosti generatora za sve tezine.
+		ftime(&vreme);
+		lst->curr->tankAll.tank.pp = lst->curr->tankAll.tank.mm = vreme.millitm;
+	}
 }
