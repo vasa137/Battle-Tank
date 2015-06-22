7b69d29748fe78824fe474997f7700a6c37837f1
vasa137 <vasa.nikola@gmail.com>
2015-06-22 17:45:30 +0200

    Update main_menu_init_matrix.c

--- main_menu_init_matrix.c
+++ main_menu_init_matrix.c
@@ -5,20 +5,21 @@ chtype lvl_matrix[dimx][dimy];
 char lvl_meni[][dimx] = { " Create New Map ", " Load Map ", " Save Map As", " Exit ", " Yes ", " No " }; // stavke menija
 
 char button[][dimx] = { " ESC : Return to menu ", " R : Turn resizing on/off ", " W : Change element to water ", " B : Change element to brick", " G : Change element to grass ",
-" SPACE : Save element on map ", };
+" SPACE : Save element on map ", " E : Eraser", " Z : Undo", " X : Redo", " SPACE : Save element on map  ", " ESC : Return to menu ", " C : Concrete "
+ };
 
 void buttons(){
-	int y1 = 55, y2 = 67;
-	int x1 = 95, i = 0, x2 = 120;
-	for (; y1 <= y2; y1 += 2)
-		mvprintw(y1, x1, button[i++]);
+ int y1 = 47, y2 = 67;
+ int x1 = 95, i = 0, x2 = 120;
+ for (; y1 <= y2; y1 += 2)
+  mvprintw(y1, x1, button[i++]);
 }
 
 void init_matrix(){
 	int i, j;
 	for (i = 0; i < 65; i++)
 		for (j = 0; j < 90; j++){
-			matrix[i][j] = '.';
+			lvl_matrix[i][j] = '.';
 		}
 }
 
@@ -41,9 +42,9 @@ void menu_content(int mainm){
 		attron(COLOR_PAIR(10));
 		mvprintw(4, 117, " M E N U : ");
 		attroff(COLOR_PAIR(10));
-		attron(COLOR_PAIR(8));
+		attron(COLOR_PAIR(8)|A_BOLD);
 		mvprintw(7, 117, meni[0]);
-		attroff(COLOR_PAIR(8));
+		attroff(COLOR_PAIR(8)|A_BOLD);
 		mvprintw(9, 117, meni[1]);
 		mvprintw(11, 117, meni[2]);
 		mvprintw(13, 117, meni[3]);
@@ -53,9 +54,9 @@ void menu_content(int mainm){
 	}
 	else{
 		mvprintw(4, 117, " LEVEL EDITOR : ");
-		attron(COLOR_PAIR(8));
+		attron(COLOR_PAIR(8)|A_BOLD);
 		mvprintw(7, 117, lvl_meni[0]);
-		attroff(COLOR_PAIR(8));
+		attroff(COLOR_PAIR(8)|A_BOLD);
 		mvprintw(9, 117, lvl_meni[1]);
 		mvprintw(11, 117, lvl_meni[2]);
 		mvprintw(13, 117, lvl_meni[3]);
@@ -76,6 +77,8 @@ void delete_menu(int y1, int x1, int y2, int x2){
 int main_menu(int mainm){
 	int  xu, xd, yd, mv = 7, ind = 0, tru = 1, limitup = 7, limitdown, i;
 	if (mainm){
+		print_border_menu(y1c-21,x1c,y2c-34,x2c);
+		print_menu_pups();
 		print_border_menu(y1c, x1c, y2c, x2c);
 		print_commands();
 	}
@@ -88,7 +91,9 @@ int main_menu(int mainm){
 			switch (getch()){
 			case KEY_UP: menu_up(&mv, limitup, &ind, 117, mainm);		 break;
 			case KEY_DOWN: menu_down(&mv, limitdown, &ind, 117, mainm);  break;
-			case ENTER: delete_menu(y1m, x1m, y2m, x2m);  /*delete command menu*/
+			case ENTER:
+				delete_menu(y1c-21,x1c,y2c-34,x2c); // vidi jos
+				delete_menu(y1m, x1m, y2m, x2m);  /*delete command menu*/
 				if (tru = option_selected(mv, mainm)){
 					print_border_menu(y1m, x1m, y2m, x2m);
 					menu_content(mainm);
@@ -97,12 +102,15 @@ int main_menu(int mainm){
 				}
 				else{
 					delete_menu(y1c, x1c, y2c, x2c);
-					if (mainm){
+					if (mainm)
+					 {
 						return 0;
 					}
-					else{
+					else
+					{
 						if (mv == limitup) return 1; //za Create new map
-						else return 0;
+						else if ((mv == 11) || (mv ==9)) return 1;
+						return 0;
 					}
 				}
 			}
@@ -119,37 +127,44 @@ int main_menu(int mainm){
 
 void level_editor(){
 	int yu = 32, xd = 32, yd = 32, xu = 32; // yu ~ y up ;  yd ~ y down; xu ~ x up; xd ~ x down     [yu]|    [xd]...[xu] |											//													[yd]|    [xd]...]xu] |
-	int i, j, dify = 0, difx = 0, tru = 1; // i,j pomocne promenljive;tru promenljiva koja prekida kretanje bloka po mapi; dify i difx vracaju visinu i sirinu bloka koji 
+	int i, j, tru = 1; // i,j pomocne promenljive;tru promenljiva koja prekida kretanje bloka po mapi; dify i difx vracaju visinu i sirinu bloka koji 
 	//treba da  se pojavi na mapi;
+	int n_undo, n_redo;
+	map *undo, *redo;
 	buffer[0] = '\0';
 	init_matrix();
 	clear();
+	element = 'b';
 	print_border_menu(2, 2, dimx + 2, dimy + 2); // screen dimensions
 	while (1){
 		if (!main_menu(0)) { delete_menu(55, 95, 67, 120); return; } // enter the menu
 		tru = 1;
+		n_undo = n_redo = 0;
+		undo = (map*)calloc(1, sizeof(map));
+		redo = (map*)calloc(1, sizeof(map));
 		buttons();
-		/*	resize_window(&difx, &dify); // prozor gde se podesavaju dimenzije bloka(elementa)*/
 		yd = yu = 32;
 		xd = xu = 32;
-		switch (element){ //  element se iscrtava na mapi
-		case 'b': print_brick_lvl(yu, xu, yd, xd); refresh(); break;
-		case 'w': print_water_lvl(yu, xu, yd, xd); refresh(); break;
-		case 'g': print_grass_lvl(yu, xu, yd, xd); refresh(); break;
-		}
+		what_to_print(yu, xu, yd, xd, element);
 		while (tru){
+				
 			switch (getch()){ // kretanje po mapi i cuvanje elementa na mapi// biranje elementa//menjanje dimenzija elementa
-			case KEY_UP:   if (lvl_can_move(yu, xu, yd, xd, 1)) move_up2x2(yu--, xu, yd--, xd);		break;
-			case KEY_DOWN: if (lvl_can_move(yu, xu, yd, xd, 2)) move_down2x2(yu++, xu, yd++, xd);	break;
-			case KEY_LEFT: if (lvl_can_move(yu, xu, yd, xd, 3))move_left2x2(yu, xu--, yd, xd--);	break;
-			case KEY_RIGHT:if (lvl_can_move(yu, xu, yd, xd, 4)) move_right2x2(yu, xu++, yd, xd++);	break;
-			case ' ': save_in_matrix(yu, xu, yd, xd, element);										break;
-			case ESC: tru = 0;																		break;
-			case 'R': case 'r': size_resize(&yu, &xu, &yd, &xd);									break;
-			case 'B': case 'b': print_brick_lvl(yu, xu, yd, xd); element = 'b';						break;
-			case 'G': case 'g': print_grass_lvl(yu, xu, yd, xd); element = 'g';						break;
-			case 'W': case 'w': print_water_lvl(yu, xu, yd, xd); element = 'w';						break;
-			case 'S': case 's': if (buffer[0] != '\0') save_it(); else save_map();					break;
+			case KEY_UP:   if (lvl_can_move(yu, xu, yd, xd, 1))		move_up2x2(yu--, xu, yd--, xd);								break;
+			case KEY_DOWN: if (lvl_can_move(yu, xu, yd, xd, 2))		move_down2x2(yu++, xu, yd++, xd);							break;
+			case KEY_LEFT: if (lvl_can_move(yu, xu, yd, xd, 3))		move_left2x2(yu, xu--, yd, xd--);							break;
+			case KEY_RIGHT:if (lvl_can_move(yu, xu, yd, xd, 4))		move_right2x2(yu, xu++, yd, xd++);							break;
+			case ' ': PUSH_unredo(&undo, &n_undo);  save_in_matrix(yu, xu, yd, xd, element);									break;																	break;
+			case ESC: tru = 0;																									break;
+			case 'R': case 'r': size_resize(&yu, &xu, &yd, &xd);																break;
+			case 'B': case 'b': print_brick_lvl(yu, xu, yd, xd); element = 'b';													break;
+			case 'G': case 'g': print_grass_lvl(yu, xu, yd, xd); element = 'g';													break;
+			case 'W': case 'w': print_water_lvl(yu, xu, yd, xd); element = 'w';													break;
+			case 'C': case 'c': print_wall_lvl(yu, xu, yd, xd); element = 'c';													break;
+			case 'E': case'e':  element = 'e';		print_eraser(yu, xu, yd, xd);	 											break;
+			case 'S': case 's': if (buffer[0] != '\0') save_it(); else save_map();												break;
+			case 'z':case 'Z': _undo_redo(&undo, &redo, &n_undo, &n_redo);		what_to_print(yu, xu, yd, xd, element);			break;
+			case 'x': case 'X': _undo_redo(&redo, &undo, &n_redo, &n_undo);		what_to_print(yu, xu, yd, xd, element);			break;
+
 			}
 		}
 	}
