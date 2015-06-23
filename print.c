7c4ead6449b080f21b39a8c51fc5aeb26dda1bf1
vasa137 <vasa.nikola@gmail.com>
2015-06-24 00:13:32 +0200

    Update print.c

--- print.c
+++ print.c
@@ -63,15 +63,70 @@ void print_eraser(int yu, int xu, int yd, int xd){
 	refresh();
 }
 
+void print_red_lvl(int yu, int xu,int yd, int xd)
+{
+	int i;
+	attron(COLOR_PAIR(7));
+	for (; yu <= yd; yu++)
+	{
+		for (i = xd; i <= xu; ++i)
+			mvaddch(yu, i, ACS_BOARD);
+	}
+	attroff(COLOR_PAIR(7));
+	//matrix[y - y1b][x - x1b] = 'h';
+}
 
-void what_to_print(int yu, int xu, int yd, int xd, chtype k){ // decides what's going to be shown on the map
+void print_blue_lvl(int yu, int xu, int yd, int xd)
+{
+		int i;
+	attron(COLOR_PAIR(14));
+	for (; yu <= yd; yu++)
+	{
+		for (i = xd; i <= xu; ++i)
+			mvaddch(yu, i, ACS_BOARD);
+	}
+	attroff(COLOR_PAIR(14));
+//	matrix[y - y1b][x - x1b] = 'h';
+}
+
+void print_white_lvl(int yu, int xu, int yd, int xd)
+{
+	int i;
+	attron(COLOR_PAIR(13));
+	for (; yu <= yd; yu++)
+	{
+		for (i = xd; i <= xu; ++i)
+			mvaddch(yu, i, ACS_BOARD);
+	}
+	attroff(COLOR_PAIR(13));
+	//matrix[y - y1b][x - x1b] = 'h';
+}
+
+void print_head_lvl(int yu, int xu, int yd, int xd)
+{
+	int i;
+	attron(COLOR_PAIR(6));
+	for (; yu <= yd; yu++)
+	{
+		for (i = xd; i <= xu; ++i)
+			mvaddch(yu, i, ACS_BOARD | A_BOLD);
+		attroff(COLOR_PAIR(6));
+	}
+}
+
+void what_to_print(int yu, int xu, int yd, int xd, chtype k)
+{ // decides what's going to be shown on the map
 	switch (k){
-	case 'b': print_brick_lvl(yu, xu, yd, xd);	break;
-	case 'w': print_water_lvl(yu, xu, yd, xd);	break;
-	case 'g': print_grass_lvl(yu, xu, yd, xd);	break;
+	case 'b': print_brick_lvl(yu, xu, yd, xd );	break;
+	case 'w': print_water_lvl(yu, xu, yd, xd );	break;
+	case 'g': print_grass_lvl(yu, xu, yd, xd );	break;
 	case '.': print_blanko_lvl(yu, xu, yd, xd); break;
-	case 'c': print_wall_lvl(yu, xu, yd, xd);	break;
-	default: print_eraser(yu, xu, yd, xd);		break;
+	case 'c': print_wall_lvl(yu, xu, yd, xd  );	break;
+	case '1': print_red_lvl(yu, xu, yd, xd   );	break;
+	case '2': print_blue_lvl(yu, xu, yd, xd  );	break;
+	case '3': print_white_lvl(yu, xu, yd, xd );	break;
+	case 'h': print_head_lvl(yu, xu, yd, xd  );	break;
+	default:  print_eraser(yu, xu, yd, xd    );	break;
 	}
 }
 
