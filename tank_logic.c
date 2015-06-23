b5274f5e17bc18b0faf9ef659f829c664e04a14b
vasa137 <vasa.nikola@gmail.com>
2015-06-23 19:00:20 +0200

    Update tank_logic.c

--- tank_logic.c
+++ tank_logic.c
@@ -1,29 +1,29 @@
 #include "tank.h"
 
 int free_place(int place){ //place=0 ispitaj sva mesta
-	int i,j,k,flag;
-	spawn_place splace[3]={ {4,20}, {4,56}, {4,72} };
-	if (place==0){
-		for(k=0;k<3;k++){
-			flag=0;
-		for( i=splace[k].y-1 ; i<=splace[k].y+1 ; i++){
-			for(j=splace[k].x-1 ; j<=splace[k].x+1; j++){
-				if(matrix[i-y1b][j-x1b]!= ' ')
-				{
-					flag=1;
-					break;
+	int i, j, k, flag;
+	spawn_place splace[3] = { { 4, 20 }, { 4, 56 }, { 4, 72 } };
+	if (place == 0){
+		for (k = 0; k<3; k++){
+			flag = 0;
+			for (i = splace[k].y - 1; i <= splace[k].y + 1; i++){
+				for (j = splace[k].x - 1; j <= splace[k].x + 1; j++){
+					if (matrix[i - y1b][j - x1b] != ' ')
+					{
+						flag = 1;
+						break;
+					}
 				}
+				if (flag) break;
 			}
-			if(flag) break;
+			if (!flag) return k + 1;
 		}
-		if(!flag) return k+1;
-	}
-	return 0;
+		return 0;
 	}
 	else{
-		for( i=splace[place-1].y-1 ; i<=splace[place-1].y+1 ; i++){
-			for(j=splace[place-1].x-1 ; j<=splace[place-1].x+1; j++){
-				if(matrix[i-y1b][j-x1b]!= ' ')
+		for (i = splace[place - 1].y - 1; i <= splace[place - 1].y + 1; i++){
+			for (j = splace[place - 1].x - 1; j <= splace[place - 1].x + 1; j++){
+				if (matrix[i - y1b][j - x1b] != ' ')
 				{
 					return 0;
 				}
@@ -36,7 +36,7 @@ int free_place(int place){ //place=0 ispitaj sva mesta
 void alloc_tank(int place){ //place=0 za nas tenk
 	List *novi;
 	int rez;
-	spawn_place splace[3]={ {4,20}, {4,56}, {4,72} };
+	spawn_place splace[3] = { { 4, 20 }, { 4, 56 }, { 4, 72 } };
 	int i, j;
 	novi = (List*)malloc(sizeof(List));
 	novi->next = NULL;
@@ -44,23 +44,28 @@ void alloc_tank(int place){ //place=0 za nas tenk
 	if (lst->first == NULL) lst->first = novi;
 	else lst->last->next = novi;
 
-	if(place==0){
+	if (place == 0){
 		lst->first->tankAll.tank.position.y = 38;// Pocetne koordinate za nas tenk, zameniti sa konstantama.
 		lst->first->tankAll.tank.position.x = 24;
+		lst->first->tankAll.projectile=(Projectile*) malloc(4*sizeof(Projectile));
+		for(i=1;i<4;i++) { novi->tankAll.projectile[i].phase = 0; novi->tankAll.projectile[i].last_object = ' '; }
 	}
 	else{
-		if(!(rez=free_place(lst->n % 3 + 1)))
-		{ novi->tankAll.tank.position.y = splace[place-1].y; novi->tankAll.tank.position.x = splace[place-1].x; }
-		else { novi->tankAll.tank.position.y =splace[rez-1].y; novi->tankAll.tank.position.x = splace[rez-1].x; }
+		if (!(rez = free_place(lst->n % 3 + 1)))
+		{
+			novi->tankAll.tank.position.y = splace[place - 1].y; novi->tankAll.tank.position.x = splace[place - 1].x;
+		}
+		else { novi->tankAll.tank.position.y = splace[rez - 1].y; novi->tankAll.tank.position.x = splace[rez - 1].x; }
+		novi->tankAll.projectile = (Projectile *) malloc (sizeof(Projectile));
 	}
 	lst->last = novi;
 	novi->tankAll.tank.position.last_move = 4;
 	novi->tankAll.tank.position.barrel = 4;
 	novi->tankAll.tank.phase = 2;
-	novi->tankAll.projectile.phase = 0;
+	novi->tankAll.projectile[0].phase = 0;
 	novi->tankAll.tank.tankDesign_v = special_tank_v;
 	novi->tankAll.tank.tankDesign_h = special_tank_h;
-	novi->tankAll.projectile.last_object = ' ';
+	novi->tankAll.projectile[0].last_object = ' ';
 	for (i = 0; i<3; i++)
 		for (j = 0; j<3; j++) novi->tankAll.tank.visit_grass[i][j] = 0;
 	create_tank(novi->tankAll.tank.position.barrel, novi->tankAll);
@@ -75,11 +80,11 @@ void free_tank(List *curr){
 	if (!prev) lst->first = curr->next;
 	else prev->next = curr->next;
 	if (curr == lst->last) lst->last = prev;
-	lstcurrcopy=lst->curr; // da ne brise travu jer globalnim lst->curr brisemo a taj lst->curr je nas tenk, curr je tenk koji se brise
-	lst->curr=curr;
-	if(lst->curr->tankAll.projectile.phase==2) delete_projectile(lst->curr->tankAll.projectile.position.y, lst->curr->tankAll.projectile.position.x, lst->curr->tankAll.projectile.last_object);
+	lstcurrcopy = lst->curr; // da ne brise travu jer globalnim lst->curr brisemo a taj lst->curr je nas tenk, curr je tenk koji se brise
+	lst->curr = curr;
+	if (lst->curr->tankAll.projectile[pridx].phase == 2) delete_projectile(lst->curr->tankAll.projectile[pridx].position.y, lst->curr->tankAll.projectile[pridx].position.x, lst->curr->tankAll.projectile[pridx].last_object);
 	delete_tank(curr->tankAll.tank.position.y, curr->tankAll.tank.position.x);
-	lst->curr=lstcurrcopy;
+	lst->curr = lstcurrcopy;
 	free(curr);
 	lst->n--;
 }
@@ -107,23 +112,24 @@ void action(int keyPressed, TankAll *current){
 			move_tank(++current->tank.position.y, current->tank.position.x, KEY_DOWN);
 		else { delete_tank(current->tank.position.y, current->tank.position.x);  create_tank(4, *current); } refresh(); break;
 
-	case ' ':       if (!(current->projectile.phase)) {
-		current->projectile.position.projectil_dir = current->tank.position.last_move;
-		current->projectile.phase = 1;
-	} break;
+	case ' ':  
+		current->projectile[pridx].last_object='t ';
+		current->projectile[pridx].position.projectil_dir = current->tank.position.last_move;
+		current->projectile[pridx].phase = 1;
+	break;
 	}
 }
 
 int can_move(int y, int x, int barrel) { // novo. Ubacio sam proveru powerupova. 
 	switch (barrel)
-	{ 
+	{
 	case 1:
-		if (((matrix[y - y1b - 2][x - x1b - 1] == ' ') || (matrix[y - y1b - 2][x - x1b - 1] == 'g') || (matrix[y - y1b - 2][x - x1b - 1] == '*')) && 
+		if (((matrix[y - y1b - 2][x - x1b - 1] == ' ') || (matrix[y - y1b - 2][x - x1b - 1] == 'g') || (matrix[y - y1b - 2][x - x1b - 1] == '*')) &&
 			((matrix[y - y1b - 2][x - x1b] == ' ') || (matrix[y - y1b - 2][x - x1b] == 'g') || (matrix[y - y1b - 2][x - x1b] == '*')) &&
 			((matrix[y - y1b - 2][x - x1b + 1] == ' ') || (matrix[y - y1b - 2][x - x1b + 1] == 'g') || (matrix[y - y1b - 2][x - x1b + 1] == '*'))) return 1;
 		else if (check_powerups(y, x, barrel))	return 1;
 		else return 0;
-		
+
 	case 2:
 		if (((matrix[y - y1b - 1][x - x1b - 2] == ' ') || (matrix[y - y1b - 1][x - x1b - 2] == 'g') || (matrix[y - y1b - 1][x - x1b - 2] == '*')) &&
 			((matrix[y - y1b][x - x1b - 2] == ' ') || (matrix[y - y1b][x - x1b - 2] == 'g') || (matrix[y - y1b][x - x1b - 2] == '*')) &&
