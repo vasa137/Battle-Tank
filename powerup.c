4a09220edc2546945efb6edc7c1f3be877d002c8
vasa137 <vasa.nikola@gmail.com>
2015-06-24 07:58:32 +0200

    Update powerup.c

--- powerup.c
+++ powerup.c
@@ -3,7 +3,7 @@
 void init_powerups(){
 	powerup.life = 1;
 	powerup.bomb = 0;
-	powerup.star = 2;
+	powerup.star = 0;
 	powerup.clock = 0;
 	powerup.shovel = 0;
 	powerup.shield = 0;
@@ -22,62 +22,174 @@ void update_powerups(char pw){
 
 int check_powerups(int y, int x, int a){
 	int i;
+	Powerup_list * temp;
+	int flag[3]={ 0, 0 ,0};
 	switch (a){
-	case 1:
+	case 1: 
 		for (i = 0; i < 6; i++){
-			if ((matrix[y - y1b - 2][x - x1b - 1] == pUps[i]) || (matrix[y - y1b - 2][x - x1b] == pUps[i]) || (matrix[y - y1b - 2][x - x1b + 1] == pUps[i])){
-				update_powerups(pUps[i]);
-				// negde da ubacimo brisanje powerupa.
-				return 1;
+			if (matrix[y - y1b - 2][x - x1b - 1] == pUps[i]) {
+				flag[0]=1;// negde da ubacimo brisanje powerupa.
 			}
-		} return 0;
+			else if ((matrix[y - y1b - 2][x - x1b - 1] == ' ') || (matrix[y - y1b - 2][x - x1b - 1] == 'g')) flag[0]=1;
+			if	(matrix[y - y1b - 2][x - x1b] == pUps[i]){
+				flag[1]=1; // negde da ubacimo brisanje powerupa.
+			}
+			else if ((matrix[y - y1b - 2][x - x1b] == ' ') || (matrix[y - y1b - 2][x - x1b] == 'g')) flag[1]=1;
+			if	(matrix[y - y1b - 2][x - x1b + 1] == pUps[i]){
+				flag[2]=1;// negde da ubacimo brisanje powerupa.
+			}
+			else if ((matrix[y - y1b - 2][x - x1b + 1] == ' ') || (matrix[y - y1b - 2][x - x1b + 1] == 'g')) flag[2]=1;
+		} 
+		for(i=0;i<=2; i++) if(flag[i]==0) return 0;
+		for (i = 0; i < 6; i++){
+			if (matrix[y - y1b - 2][x - x1b - 1] == pUps[i]) {
+				if(lst->curr==lst->first) update_powerups(pUps[i]);
+				temp=which_powerup(y-2,x-1);
+				free_powerup(temp); 
+				// negde da ubacimo brisanje powerupa.
+			}
+
+			if	(matrix[y - y1b - 2][x - x1b] == pUps[i]){
+				if(lst->curr==lst->first) update_powerups(pUps[i]);
+				temp=which_powerup(y-2,x);
+				free_powerup(temp);
+		 // negde da ubacimo brisanje powerupa.
+			}
+
+			if	(matrix[y - y1b - 2][x - x1b + 1] == pUps[i]){
+				if(lst->curr==lst->first) update_powerups(pUps[i]);
+				temp=which_powerup(y-2,x+1);
+				free_powerup(temp); 
+				// negde da ubacimo brisanje powerupa.
+			}
+
+		} 
+		return 1;
 	case 2:
 		for (i = 0; i < 6; i++){
-			if ((matrix[y - y1b - 1][x - x1b - 2] == pUps[i]) || (matrix[y - y1b][x - x1b - 2] == pUps[i]) || (matrix[y - y1b + 1][x - x1b - 2] == pUps[i])){
-				update_powerups(pUps[i]);
-				// negde da ubacimo brisanje powerupa.
-				return 1;
+		if (matrix[y - y1b - 1][x - x1b - 2] == pUps[i]) {
+				flag[0]=1;// negde da ubacimo brisanje powerupa.
 			}
-		} return 0;
+		else if ((matrix[y - y1b - 1][x - x1b - 2] == ' ') || (matrix[y - y1b - 1][x - x1b - 2] == 'g')) flag[0]=1;
+			if	(matrix[y - y1b][x - x1b - 2] == pUps[i]){
+				
+				flag[1]=1; // negde da ubacimo brisanje powerupa.
+			}
+			else if ((matrix[y - y1b][x - x1b - 2] == ' ') || (matrix[y - y1b][x - x1b - 2] == 'g')) flag[1]=1;
+			if	(matrix[y - y1b + 1][x - x1b - 2] == pUps[i]){
+				flag[2]=1;// negde da ubacimo brisanje powerupa.
+			}
+			else if ((matrix[y - y1b + 1][x - x1b - 2] == ' ') || (matrix[y - y1b + 1][x - x1b - 2] == 'g')) flag[2]=1;
+	}
+	for(i=0;i<=2; i++) if(flag[i]==0) return 0;
+	for (i = 0; i < 6; i++){
+		if (matrix[y - y1b - 1][x - x1b - 2] == pUps[i]) {
+				if(lst->curr==lst->first) update_powerups(pUps[i]);
+				temp=which_powerup(y-1,x-2);
+				free_powerup(temp); 
+			}
+			if	(matrix[y - y1b][x - x1b - 2] == pUps[i]){
+				if(lst->curr==lst->first) update_powerups(pUps[i]);
+				temp=which_powerup(y,x-2);
+				free_powerup(temp);
+			}
+			else if ((matrix[y - y1b][x - x1b - 2] == ' ') || (matrix[y - y1b][x - x1b - 2] == 'g')) flag[1]=1;
+			if	(matrix[y - y1b + 1][x - x1b - 2] == pUps[i]){
+				if(lst->curr==lst->first) update_powerups(pUps[i]);
+				temp=which_powerup(y+1,x-2);
+				free_powerup(temp);	
+			}
+	}
+	return 1;
 	case 3:
 		for (i = 0; i < 6; i++){
-			if ((matrix[y - y1b - 1][x - x1b + 2] == pUps[i]) || (matrix[y - y1b][x - x1b + 2] == pUps[i]) || (matrix[y - y1b + 1][x - x1b + 2] == pUps[i])){
-				update_powerups(pUps[i]);
-				// negde da ubacimo brisanje powerupa.
-				return 1;
+		if (matrix[y - y1b - 1][x - x1b + 2] == pUps[i]) {
+				flag[0]=1; // negde da ubacimo brisanje powerupa.
 			}
-		} return 0;
+		else if ((matrix[y - y1b - 1][x - x1b + 2] == ' ') || (matrix[y - y1b - 1][x - x1b + 2] == 'g')) flag[0]=1;
+			if	(matrix[y - y1b][x - x1b + 2] == pUps[i]){
+				flag[1]=1;// negde da ubacimo brisanje powerupa.
+			}
+			else if ((matrix[y - y1b][x - x1b + 2] == ' ') || (matrix[y - y1b][x - x1b + 2] == 'g')) flag[1]=1;
+			if	(matrix[y - y1b + 1][x - x1b + 2] == pUps[i]){
+				flag[2]=1; // negde da ubacimo brisanje powerupa.
+			}
+			else if ((matrix[y - y1b + 1][x - x1b + 2] == ' ') || (matrix[y - y1b + 1][x - x1b + 2] == 'g')) flag[2]=1;
+	} 
+		for(i=0;i<=2; i++) if(flag[i]==0) return 0;
+			for (i = 0; i < 6; i++){
+		if (matrix[y - y1b - 1][x - x1b + 2] == pUps[i]) {
+				if(lst->curr==lst->first) update_powerups(pUps[i]);
+				temp=which_powerup(y-1,x+2);
+				free_powerup(temp); // negde da ubacimo brisanje powerupa.
+			}
+			if	(matrix[y - y1b][x - x1b + 2] == pUps[i]){
+				if(lst->curr==lst->first) update_powerups(pUps[i]);
+				temp=which_powerup(y,x+2);
+				free_powerup(temp); // negde da ubacimo brisanje powerupa.
+			}
+			if	(matrix[y - y1b + 1][x - x1b + 2] == pUps[i]){
+				if(lst->curr==lst->first) update_powerups(pUps[i]);
+				temp=which_powerup(y+1,x+2);
+				free_powerup(temp);
+			}
+	} 
+	return 1;
 	case 4:
 		for (i = 0; i < 6; i++){
-			if ((matrix[y - y1b + 2][x - x1b - 1] == pUps[i]) || (matrix[y - y1b + 2][x - x1b] == pUps[i]) || (matrix[y - y1b + 2][x - x1b + 1] == pUps[i])){
-				update_powerups(pUps[i]);
-				// negde da ubacimo brisanje powerupa.
-				return 1;
+			if (matrix[y - y1b + 2][x - x1b - 1] == pUps[i]) {
+				flag[0]=1;// negde da ubacimo brisanje powerupa.
 			}
-		} return 0;
+			else if ((matrix[y - y1b + 2][x - x1b - 1] == ' ') || (matrix[y - y1b + 2][x - x1b - 1] == 'g')) flag[0]=1;
+			if	(matrix[y - y1b + 2][x - x1b] == pUps[i]){
+				flag[1]=1;// negde da ubacimo brisanje powerupa.
+			}
+			else if ((matrix[y - y1b + 2][x - x1b] == ' ') || (matrix[y - y1b + 2][x - x1b] == 'g')) flag[1]=1;
+			if	(matrix[y - y1b + 2][x - x1b + 1] == pUps[i]){
+				flag[2]=1;// negde da ubacimo brisanje powerupa.
+			}
+			else if ((matrix[y - y1b + 2][x - x1b + 1] == ' ') || (matrix[y - y1b + 2][x - x1b + 1] == 'g')) flag[2]=1;
+		}	 
+		for(i=0;i<=2; i++) if(flag[i]==0) return 0;
+		for (i = 0; i < 6; i++){
+			if (matrix[y - y1b + 2][x - x1b - 1] == pUps[i]) {
+				if(lst->curr==lst->first) update_powerups(pUps[i]);
+				temp=which_powerup(y+2,x-1);
+				free_powerup(temp);
+			}
+			if	(matrix[y - y1b + 2][x - x1b] == pUps[i]){
+				if(lst->curr==lst->first) update_powerups(pUps[i]);
+				temp=which_powerup(y+2,x);
+				free_powerup(temp);
+			}
+			if	(matrix[y - y1b + 2][x - x1b + 1] == pUps[i]){
+				if(lst->curr==lst->first) update_powerups(pUps[i]);
+				temp=which_powerup(y+2,x+1);
+				free_powerup(temp);
+			}
+		}	
+		return 1;
 	}
 }
 
 void execute_powerups(clock_t *pw_shield_start, clock_t *pw_clock_start, clock_t *pw_shovel_start, unsigned long int*random_pup_gen){
 	int i, j, check, x, y;
-	List *current, *temp;
-	random_pup_gen++;
-	if (*random_pup_gen == 777000) *random_pup_gen = 0, rand_pup_gen();
+	List *current,*temp;
+	(*random_pup_gen)++;
+	if (*random_pup_gen == 770000) *random_pup_gen = 0, rand_pup_gen();
 	if (powerup.bomb){
-		for (current = lst->first->next; current != NULL;){
-			check = 0;
+		for (current = lst->first->next; current != NULL; ){
+			check=0;
 			for (i = 0; i < 3; i++) // proverava da li je u travi.
 				for (j = 0; j < 3; j++)
-					if (current->tankAll.tank.visit_grass[i][j])
-					{
-						check = 1; break;
-					}
+					if(current->tankAll.tank.visit_grass[i][j])
+					{ check = 1; break; }
 			if (!check){
-				temp = current;
-				current = current->next;
+				temp=current; 
+				current=current->next;
 				free_tank(temp);
 			}// ako nije u travi brise ga.
-			else current = current->next;
+			else current=current->next;
 		}
 		powerup.bomb = 0;
 	}
@@ -87,16 +199,16 @@ void execute_powerups(clock_t *pw_shield_start, clock_t *pw_clock_start, clock_t
 			*pw_clock_start = clock();
 			powerup.clock = 2;
 			for (current = lst->first->next; current != NULL; current = current->next)
-				current->tankAll.tank.phase = 5;
+			current->tankAll.tank.phase = 5;
 		}
-		if (((clock() - *pw_clock_start) * 1000 / CLOCKS_PER_SEC) > PW_DURATION){ //  konstanta! PW_DURATION
+		if (((clock()-*pw_clock_start)*1000/CLOCKS_PER_SEC) >  PW_DURATION){
 			for (current = lst->first->next; current != NULL; current = current->next)
 				current->tankAll.tank.phase = 2;
 			powerup.clock = 0;
-		}
+		}	
 	}
 
-	if (powerup.shovel){ // ===========
+	if (powerup.shovel){
 		if (powerup.shovel == 1){
 			*pw_clock_start = clock();
 			x = 41;
@@ -117,14 +229,15 @@ void execute_powerups(clock_t *pw_shield_start, clock_t *pw_clock_start, clock_t
 			for (x = 41; x < 50; x++) print_head(y, x);
 			powerup.shovel = 0;
 		}
+	
 	}
-
+	
 	if (powerup.shield){ // imam if u funkciji collision: if (!((temp == lst->first) && powerup.shield)) free_tank(temp);
 		if (powerup.shield == 1){
 			*pw_shield_start = clock();
 			powerup.shield = 2;
 		}
-		if (((clock() - *pw_shield_start) * 1000 / CLOCKS_PER_SEC) > PW_DURATION){//  konstanta! PW_DURATION
+		if (((clock()-*pw_shield_start)*1000/CLOCKS_PER_SEC) >  PW_DURATION){
 			powerup.shield = 0;
 		}
 	}
@@ -132,9 +245,63 @@ void execute_powerups(clock_t *pw_shield_start, clock_t *pw_clock_start, clock_t
 
 int stars()
 {
-	switch (powerup.star){
+	switch(powerup.star){
 	case 0: return 1;
 	case 1: return 2;
 	case 2: case 3: return 4;
 	}
+}
+
+void alloc_powerup(chtype type, int y, int x)
+{
+	Powerup_list* novi;
+	novi=(Powerup_list *) malloc (sizeof(Powerup_list));
+	novi->next=NULL;
+	if(Plst->first==NULL) Plst->first=novi;
+	else Plst->last->next=novi;
+	Plst->last=novi;
+	novi->type=type;
+	novi->position_y=y;
+	novi->position_x=x;
+}
+
+void free_powerup(Powerup_list* curr)
+{
+	Powerup_list* temp=Plst->first, *prev = NULL;
+	int i,j;
+	while (temp != curr){
+		prev = temp;
+		temp = temp->next;
+	}
+	if (!prev) Plst->first = curr->next;
+	else prev->next = curr->next;
+	if (curr == Plst->last) Plst->last = prev;
+	for (j = (curr->position_y - 1); j < (curr->position_y + 2); j++){
+		for (i = (curr->position_x - 1); i < (curr->position_x + 2); i++){
+			matrix[j - 2][i - 2] = ' ';
+			print_blanko(j,i);
+		}
+	}
+	free(curr);
+}
+
+Powerup_list * which_powerup(int y, int x){
+	Powerup_list * temp=Plst->first;
+	while(temp!=NULL){
+		if ((y == temp->position_y || y == temp->position_y + 1 || y == temp->position_y - 1) && (x == temp->position_x|| x == temp->position_x + 1 || x == temp->position_x - 1)) return temp;
+		temp=temp->next;
+	}
+}
+
+
+void delete_powerup_list()
+{
+	Powerup_list * temp;
+	while(Plst->first){
+		temp=Plst->first;
+		Plst->first=Plst->first->next;
+		free(temp);
+	}
+	Plst->last=NULL;
+	free(Plst);
 }
