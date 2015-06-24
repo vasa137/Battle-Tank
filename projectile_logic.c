4e84c43f83d487559c2bdf4ac4631bf431441e46
vasa137 <vasa.nikola@gmail.com>
2015-06-24 07:59:06 +0200

    Update projectile_logic.c

--- projectile_logic.c
+++ projectile_logic.c
@@ -88,7 +88,9 @@ phase of projectile*/
 
 barrier can_fly(int y, int x)
 {
-	barrier ret; //return
+	barrier ret; 
+	List * temp;
+	int i;//return
 	ret.obs = matrix[y - y1b][x - x1b];
 	switch (ret.obs){
 	case ' ': case 'w':
@@ -97,11 +99,19 @@ barrier can_fly(int y, int x)
 	case 'g': 
 		ret.flag = 2;
 		break;
-	case 'b': case 'e': case 'c': case 't': 
+	case 'b': case 'e': case 'c': case 't': case 'x': case 'h' : case 'l': case 's': case 'q': case 'a' : case 'y':
 		ret.flag = 0;
 		break;
-	case '*': ret.obs='t';
-		ret.flag=3;
+	case '*':   
+		temp=NULL;
+		for(i=0;i<4;i++) { if(lst->first->tankAll.projectile[i].phase==2) if ((y == lst->first->tankAll.projectile[i].position.y) && (x==lst->first->tankAll.projectile[i].position.x)) temp=lst->first;}
+		if(temp==lst->first && lst->first==lst->curr){
+			ret.obs='t';
+			ret.flag=3;
+			break;      //brute force
+		}
+		else
+		ret.flag=0;
 		break;
 	}
 	return ret;
@@ -110,6 +120,8 @@ barrier can_fly(int y, int x)
 
 void collision(int y, int x, int projectil_dir, char object){ // saljes mi koordinate udara.
 	List *temp;
+	Powerup_list * pom;
+	int pridxcopy, i;
 	switch (object){
 	case 'b':
 		switch (projectil_dir){
@@ -138,19 +150,101 @@ void collision(int y, int x, int projectil_dir, char object){ // saljes mi koord
 		break;
 	case 't':
 		temp = which_tank(y, x);
-		if (!((temp == lst->first) && powerup.shield)) free_tank(temp);
+		if((lst->curr!=lst->first) && (temp!=lst->first)) break; // friendly-fire
+		if(temp==lst->first){
+			if (powerup.shield) break;
+			if (powerup.life>1){
+				delete_tank(lst->first->tankAll.tank.position.y,lst->first->tankAll.tank.position.x);
+				lst->first->tankAll.tank.position.y = 65;// Pocetne koordinate za nas tenk, zameniti sa konstantama.
+				lst->first->tankAll.tank.position.x = 24;
+				lst->first->tankAll.tank.position.last_move = 1;
+				lst->first->tankAll.tank.position.barrel = 1;
+				for(i=1;i<4;i++) { 
+					if (lst->curr->tankAll.projectile[pridx].phase == 2){
+					delete_projectile(lst->curr->tankAll.projectile[i].position.y, lst->curr->tankAll.projectile[i].position.x, lst->curr->tankAll.projectile[i].last_object);
+				}	
+				lst->first->tankAll.projectile[i].phase = 0; 
+				lst->first->tankAll.projectile[i].last_object = ' ';
+				}
+				create_tank(1,lst->first->tankAll);
+				powerup.life--;
+			}
+			else{
+				delete_tank(lst->first->tankAll.tank.position.y,lst->first->tankAll.tank.position.x);
+				strcpy(map_name,"gameover.map");
+				create_map(map_name);
+				_sleep(2300);
+				powerup.life--;
+				break;
+			}
+		}
+		else free_tank(temp);
+		break;
+	case 'x': case 'l': case 's': case 'q': case 'a' : case 'y':
+		pom=which_powerup(y,x);
+		free_powerup(pom);
+		break;
+	case 'c':
+		if(lst->curr->tankAll.tank.type==1){
+			switch (projectil_dir){
+		case 1://up 
+			if (matrix[y - y1b][x - x1b - 1] == 'c' && (matrix[y - y1b + 1][x - x1b - 1] != 'c')) print_blanko(y, x - 1);
+			if (matrix[y - y1b][x - x1b] == 'c') print_blanko(y, x);
+			if (matrix[y - y1b][x - x1b + 1] == 'c' && (matrix[y - y1b + 1][x - x1b + 1] != 'c')) print_blanko(y, x + 1);
+			break;
+		case 2: // left.
+			if ((matrix[y - y1b - 1][x - x1b] == 'c') && (matrix[y - y1b - 1][x - x1b + 1] != 'c')) print_blanko(y - 1, x);
+			if (matrix[y - y1b][x - x1b] == 'c') print_blanko(y, x);
+			if ((matrix[y - y1b + 1][x - x1b] == 'c') && (matrix[y - y1b + 1][x - x1b + 1] != 'c'))print_blanko(y + 1, x);
+			break;
+		case 3: //right.
+			if ((matrix[y - y1b - 1][x - x1b] == 'c') && (matrix[y - y1b - 1][x - x1b - 1] != 'c')) print_blanko(y - 1, x);
+			if (matrix[y - y1b][x - x1b] == 'c') print_blanko(y, x);
+			if ((matrix[y - y1b + 1][x - x1b] == 'c') && (matrix[y - y1b + 1][x - x1b - 1] != 'c')) print_blanko(y + 1, x);
+			break;
+		case 4:
+			if ((matrix[y - y1b][x - x1b - 1] == 'c') && (matrix[y - y1b - 1][x - x1b - 1] != 'c')) print_blanko(y, x - 1);
+			if (matrix[y - y1b][x - x1b] == 'c') print_blanko(y, x);
+			if (matrix[y - y1b][x - x1b + 1] == 'c' && (matrix[y - y1b - 1][x - x1b + 1] != 'c')) print_blanko(y, x + 1);
+			break;
+		}
+	}
+	break; // za beton, kada budemo imali strukturu.
+	case '*':
+		pridxcopy=pridx;
+		temp=which_projectile(y,x);
+		if(lst->curr!=temp){
+			delete_projectile(temp->tankAll.projectile[pridx].position.y,temp->tankAll.projectile[pridx].position.x,temp->tankAll.projectile[pridx].last_object);
+			temp->tankAll.projectile[pridx].phase=0;
+		}// za metak o metak, gledamo razlicite strane i gledamo da li se sudaraju, pazimo na mimoilazenje.
+		pridx=pridxcopy;
 		break;
 
-	case 'c':; // za beton, kada budemo imali strukturu.
-	case '*':; // za metak o metak, gledamo razlicite strane i gledamo da li se sudaraju, pazimo na mimoilazenje.
+	case 'h':
+		strcpy(map_name,"gameover.map");
+				create_map(map_name);
+				_sleep(2300);
+		break;
 	}
 
 }
 
 List* which_tank(int y, int x){
 	List *temp = lst->first;
+	int i;
 	while (temp){
 		if ((y == temp->tankAll.tank.position.y || y == temp->tankAll.tank.position.y + 1 || y == temp->tankAll.tank.position.y - 1) && (x == temp->tankAll.tank.position.x || x == temp->tankAll.tank.position.x + 1 || x == temp->tankAll.tank.position.x - 1)) return temp;
+		temp = temp->next;
+	}
+}
+
+List * which_projectile(int y, int x){
+	List *temp = lst->first;
+	while (temp){
+		if(temp!=lst->curr){
+		if(temp==lst->first){ for(pridx=0;pridx<4;pridx++) if(temp->tankAll.projectile[pridx].phase==2) if ((y == temp->tankAll.projectile[pridx].position.y) && (x==temp->tankAll.projectile[pridx].position.x)) return temp;}
+		else { pridx=0 ;  if(temp->tankAll.projectile[pridx].phase==2)  if ((y == temp->tankAll.projectile[0].position.y) && (x==temp->tankAll.projectile[0].position.x)) return temp; }
+		}
 		temp = temp->next;
 	}
 }
