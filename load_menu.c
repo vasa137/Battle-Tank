968f2abc152829a65328055a839b74d7c5adfca7
vasa137 <vasa.nikola@gmail.com>
2015-06-27 00:34:52 +0200

    Update load_menu.c

--- load_menu.c
+++ load_menu.c
@@ -33,17 +33,22 @@ void free_book(char** book)
 int print_a_page(char **book, int c_page, int limitdown, int limitup, int per_page)
 {
 	int i, j = limitup;
+	char name[100];
 	for (i = (c_page - 1)*per_page; book[i] != NULL; i++)
 	{
-		mvprintw(j, 113, "%s", book[i]);
+		strcpy(name,book[i]);
+		name[strlen(name)-4]='\0';
+		mvprintw(j, 113, "%s", name);
 		j += 2;
 		if (j == limitdown) break;
 	}
 	mvprintw(38, 117, "         ");
 	mvprintw(38, 117, "PAGE %d", c_page);
-	attron(COLOR_PAIR(8));
-	mvprintw(8, 113, "%s", book[(c_page-1)*per_page]);
-	attroff(COLOR_PAIR(8));
+	strcpy(name,book[(c_page-1)*per_page]);
+	name[strlen(name)-4]='\0';
+	attron(COLOR_PAIR(8)| A_BOLD);
+	mvprintw(8, 113, "%s", name);
+	attroff(COLOR_PAIR(8)| A_BOLD);
 	refresh();
 	return j;
 }
@@ -51,12 +56,17 @@ int print_a_page(char **book, int c_page, int limitdown, int limitup, int per_pa
 void lmenu_up(char **book, int *mv, int limit, int *ind, int from)
 {
 	int i;
+	char name[100];
 	if (*mv == limit) return;
+	strcpy(name,book[--(*ind)]);
+	name[strlen(name)-4]='\0';
 	*mv = *mv - 2;
-	attron(COLOR_PAIR(8));
-	mvprintw(*mv, from, "%s  ", book[--(*ind)]);
-	attroff(COLOR_PAIR(8));
-	mvprintw(*mv + 2, from, "%s  ", book[*ind + 1]);
+	attron(COLOR_PAIR(8)| A_BOLD);
+	mvprintw(*mv, from, "%s  ", name);
+	attroff(COLOR_PAIR(8)| A_BOLD);
+	strcpy(name,book[*ind + 1]);
+	name[strlen(name)-4]='\0';
+	mvprintw(*mv + 2, from, "%s  ", name);
 	refresh();
 
 }
@@ -64,25 +74,31 @@ void lmenu_up(char **book, int *mv, int limit, int *ind, int from)
 void lmenu_down(char **book, int *mv, int limit, int *ind, int from)
 {
 	int i;
+	char name[100];
 	if (*mv == limit) return;
 	*mv = *mv + 2;
-	attron(COLOR_PAIR(8));
-	mvprintw(*mv, from, "%s  ", book[++(*ind)]);
-	attroff(COLOR_PAIR(8));
-	mvprintw(*mv - 2, from, "%s  ", book[*ind - 1]);
+	strcpy(name,book[++(*ind)]);
+	name[strlen(name)-4]='\0';
+	attron(COLOR_PAIR(8)| A_BOLD);
+	mvprintw(*mv, from, "%s  ", name);
+	attroff(COLOR_PAIR(8)| A_BOLD);
+	strcpy(name, book[*ind - 1]);
+	name[strlen(name)-4]='\0';
+	mvprintw(*mv - 2, from, "%s  ",name);
 	refresh();
 } // kretanje po meniju
 
 int load_maps(int decide){
 	char **book = (char**)calloc(1, sizeof(char*));
-	FILE *saved_maps; 
+	FILE *saved_maps,*saved_games; 
 	int pages, num = 0, per_page = 10, c_page = 1;
 	int x1 = 100, y1 = 2, x2 = 145, y2 = 45;
 	int limitup = 8, limitdown = 28, mv = 8, i = 0, j = 6;
 	int from = 113;
-	if(!(saved_maps = fopen("saved_maps.txt", "r"))) return 1; // if the file doesn't exist, return
-
-
+	switch(decide){
+		case 0 :case 1:	if (!(saved_maps = fopen("saved_maps.txt", "r"))) return 1;   break;  //ne zaboravi break, if the file doesn't exist, return
+		case 2: if (!(saved_maps = fopen("saved_games.txt", "r"))) return 1; break;
+	}
 	while (!feof(saved_maps)) // inserts every map name into an array
 	{
 		book[num] = (char*)calloc(20, sizeof(char));
@@ -100,7 +116,8 @@ int load_maps(int decide){
 	fclose(saved_maps);
 	pages = (num - 1) / per_page + 1;
 	print_border_menu(y1, x1, y2, x2);
-	mvprintw(4, 115, "L O A D   M A P");
+	if(decide==2) mvprintw(4, 115, "L O A D   G A M E");
+	else mvprintw(4, 115, "L O A D   M A P");
 	mvprintw(42, 102, "ARROW LEFT/RIGHT : navigate through pages");
 	mvprintw(44, 102, "ARROW UP/RIGHT : navigate through files");
 	refresh();
@@ -110,19 +127,22 @@ int load_maps(int decide){
 		j = print_a_page(book, c_page, limitdown, limitup, per_page) - 2;
 		while (1){
 			switch (getch()){
-			case KEY_UP: lmenu_up(book, &mv, limitup, &i, from); break;
-			case KEY_DOWN: lmenu_down(book, &mv, j, &i, from); break;
-			case KEY_LEFT: if (c_page > 1) 
+			case KEY_UP: PlaySound(TEXT("Menu_move.wav"),NULL, SND_ASYNC); lmenu_up(book, &mv, limitup, &i, from); break;
+			case KEY_DOWN: PlaySound(TEXT("Menu_move.wav"),NULL, SND_ASYNC); lmenu_down(book, &mv, j, &i, from); break;
+			case KEY_LEFT:PlaySound(TEXT("Menu_move.wav"),NULL, SND_ASYNC); if (c_page > 1) 
 			{
 				delete_menu(5, 101, 38, 144); j = print_a_page(book, --c_page, limitdown, limitup, per_page) - 2; mv = 8; i = (c_page - 1)*per_page;
 			} break;
-			case KEY_RIGHT: if (c_page < pages) {
+			case KEY_RIGHT: PlaySound(TEXT("Menu_move.wav"),NULL, SND_ASYNC); if (c_page < pages) {
 				delete_menu(5, 101, 38, 144);  j = print_a_page(book, ++c_page, limitdown, limitup, per_page) - 2; mv = 8; i = (c_page - 1)*per_page;
 			} break;
-			case ENTER: if (decide) { strcpy(buffer, book[i]); load_matrix(book[i]); print_matrix(); free_book(book);  delete_menu(y1, x1, y2, x2); return 0; }
-						else { clear(); custom_map(book[i]); free_book(book); delete_menu(y1, x1, y2, x2); return 1; }
-
-
+			case ENTER: PlaySound(TEXT("select.wav"),NULL, SND_ASYNC); if (decide==1) { strcpy(buffer, book[i]); load_matrix(book[i]); print_matrix(); free_book(book);  delete_menu(y1, x1, y2, x2); return 0; }
+						else if (!decide) { clear(); custom_map(book[i]); free_book(book); delete_menu(y1, x1, y2, x2);  return 1; }
+						else if(decide==2){
+							load_game(book[i]);
+							delete_menu(y1, x1, y2, x2);
+							return 0;
+						}
 				break;
 			case ESC: delete_menu(y1, x1, y2, x2); return 1; break;
 			}
